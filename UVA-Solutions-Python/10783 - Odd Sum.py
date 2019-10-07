2bd017cca9f70155dd0df6ffebe936d45403ec79
ihjohny <ihjony23@gmail.com>
2019-10-07 19:02:23 +0600

    O(1) solution using equation

---
+++ UVA-Solutions-Python/10783 - Odd Sum.py
@@ -0,0 +1,11 @@
+﻿testCase = int(input())
+for i in range(testCase):
+	a = int(input())
+	b = int(input())
+	if a%2 == 0:
+		a = a+1
+	if b%2 == 0:
+		b = b-1
+	n = ((b-a)/2)+1
+	ans = (n/2)*(a+b)
+	print('Case ',(i+1),': ',int(ans),sep="")
