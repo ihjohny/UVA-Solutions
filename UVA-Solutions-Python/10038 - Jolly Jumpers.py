d3e0c1b8131140dc993785bf09ef05515c2123a7
ihjohny <ihjony23@gmail.com>
2019-10-08 15:11:35 +0600

    sum with list sliceing

---
+++ UVA-Solutions-Python/10038 - Jolly Jumpers.py
@@ -0,0 +1,14 @@
+﻿while True:
+	try:
+		numbers = list(map(int, input().split()))
+		dif = [0 for i in range(3009)]
+		flag = True
+		for i in range(1,numbers[0]):
+			a = abs(numbers[i] - numbers[i+1])
+			dif[a] = 1
+		if(sum(dif[1:numbers[0]]) == numbers[0]-1):
+			print("Jolly")
+		else:
+			print("Not jolly")
+	except EOFError:
+		break;
