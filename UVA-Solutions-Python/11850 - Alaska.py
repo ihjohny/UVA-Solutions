8655a3fbfd202f4eb6e4a7a4fa4c5b75dbdc8ca3
ihjohny <ihjony23@gmail.com>
2019-10-14 07:21:02 +0600

    get any point from 1 to 1322 with max difference 200

---
+++ UVA-Solutions-Python/11850 - Alaska.py
@@ -0,0 +1,36 @@
+﻿# 11850 - Alaska
+while True:
+	n = int(input())
+	if n == 0:
+		break
+	station = []
+	for i in range(n):
+		a = int(input())
+		station.append(a)
+	station.sort()
+	flag = True
+	total = 0
+
+	for i in range(n-1):
+		if (station[i+1] - station[i]) > 200:
+			flag = False
+			break
+		total = total + (station[i+1] - station[i])
+	
+	if (station[0] - 0) > 200:
+		flag = False
+	elif station[0] != 0:
+		total = total + (station[0] - 0)		
+	
+	if (1422 - station[n-1]) > 100:
+		flag = False
+	elif station[n-1] != 1422:
+		total = total + (1422 - station[n-1])	
+
+	if total != 1422:
+		flag = False
+		
+	if flag == True:
+		print("POSSIBLE")
+	else:
+		print("IMPOSSIBLE")
